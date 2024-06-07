bef502ef93636149d5992712ac936ef60dd39639
Shivangi Baranwal <shivangibaranwal15@gmail.com>
2024-06-07 21:19:20 +0530

    Homepage1

---
+++ frontend/src/pages/Main/Components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    const style = {
+        backgroundImage: `url("tree.jpg")`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh', // Adjust as needed
+        padding:0
+      };
+  return (
+   <div style={style}>hello about</div>
+  )
+}
+
+export default About
