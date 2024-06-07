4de8c30f838215ebd641302891f473659f767414
anshika040 <anshilashukla3466@gamil.com>
2024-06-07 15:02:10 +0530

    signup and login page

--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,22 +1,19 @@
-import React from "react";
-
-import { useState } from "react";
-import "./Login.css";
-import {
-  useSignInWithEmailAndPassword,
-  useSignInWithGoogle,
-} from "react-firebase-hooks/auth";
+import React, { useState } from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import GoogleButton from "react-google-button";
 import { Link, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
+import "./Login.css";
 
 const Login = () => {
   const navigate = useNavigate();
   const [signInWithGoogle, googleuser, googleloading, googleerror] =
     useSignInWithGoogle(auth);
+
   const handleGoogleSignIn = () => {
     signInWithGoogle();
   };
+
   if (googleuser) {
     navigate("/home");
     console.log(googleuser);
@@ -29,83 +26,86 @@ const Login = () => {
   }
 
   const [action, setAction] = useState("Sign Up");
-  return (
-    <div className="login-container">
-      <div className="container">
-        <div className="header">
-          <div className="text">{action}</div>
-          <div className="underline"></div>
-        </div>
 
-        <div className="inputs">
-          {action === "Login" ? (
+  return (
+    <div className="page-container">
+      <div className="image-container">
+        <img src="./login-image.avif" alt="Placeholder Image" />
+      </div>
+      <div className="login-container">
+        <div className="container">
+          <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+          </div>
+
+          <div className="inputs">
+            {action === "Login" ? (
+              <div></div>
+            ) : (
+              <div className="input">
+                <input placeholder="Your Name" type="text" />
+              </div>
+            )}
+
+            <div className="input">
+              <input type="email" placeholder="Your Email" />
+            </div>
+
+            <div className="input">
+              <input type="password" placeholder="Your Password" />
+            </div>
+          </div>
+          {action === "Sign Up" ? (
             <div></div>
           ) : (
-            <div className="input">
-              <input placeholder="Your Name" type="text" />
+            <div className="forget-password">
+              Forgot Password?
+              <span>Click Here</span>
             </div>
           )}
 
-          <div className="input">
-            <input type="email" placeholder="Your Email"></input>
-          </div>
-
-          <div className="input">
-            <input type="password" placeholder="Your Password"></input>
-          </div>
-        </div>
-        {action === "Sign Up" ? (
-          <div></div>
-        ) : (
-          <div className="forget-password">
-            Forgot Password?
-            <span>Click Here</span>
-          </div>
-        )}
-
-        <div className="submit-container">
-          <div
-            className={action === "Login" ? "submit gray" : "submit"}
-            onClick={() => {
-              setAction("SignUp");
-            }}
-          >
-            Sign Up
-          </div>
-          <div
-            className={action === "Sign Up" ? "submit gray" : "submit"}
-            onClick={() => {
-              setAction("Login");
-            }}
-          >
-            Login
+          <div className="submit-container">
+            <div
+              className={action === "Login" ? "submit gray" : "submit"}
+              onClick={() => {
+                setAction("SignUp");
+              }}
+            >
+              Sign Up
+            </div>
+            <div
+              className={action === "Sign Up" ? "submit gray" : "submit"}
+              onClick={() => {
+                setAction("Login");
+              }}
+            >
+              Login
+            </div>
           </div>
         </div>
-      </div>
-      <div className="form-container">
-        <div className="form-box">
-          <hr />
+        <div className="form-container">
+          <div className="form-box">
+            <hr />
+            <div>
+              <GoogleButton
+                className="g-btn"
+                type="light"
+                onClick={handleGoogleSignIn}
+              />
+            </div>
+          </div>
           <div>
-            <GoogleButton
-              className="g-btn"
-              type="light"
-              onClick={handleGoogleSignIn}
-            />
+            <Link
+              to="/signup"
+              style={{
+                textDecoration: "none",
+                color: "var(--twitter-color)",
+                fontWeight: "600",
+                marginLeft: "5px",
+              }}
+            ></Link>
           </div>
-        </div>
-        <div>
-          Don't have an account?
-          <Link
-            to="/signup"
-            style={{
-              textDecoration: "none",
-              color: "var(--twitter-color)",
-              fontWeight: "600",
-              marginLeft: "5px",
-            }}
-          >
-            Sign up
-          </Link>
         </div>
       </div>
     </div>
--- frontend/src/pages/Login/Signup.js
+++ frontend/src/pages/Login/Signup.js
@@ -52,82 +52,87 @@ const Signup = () => {
 
   const [action, setAction] = useState("Sign Up");
   return (
-    <div className="login-container">
-      <div className="container">
-        <div className="header">
-          <div className="text">{action}</div>
-          <div className="underline"></div>
-        </div>
+    <div className="page-container">
+      <div className="image-container">
+        <img src="./login-image.avif" alt="Placeholder Image" />
+      </div>
+      <div className="login-container">
+        <div className="container">
+          <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+          </div>
 
-        <div className="inputs">
-          {action === "Login" ? (
+          <div className="inputs">
+            {action === "Login" ? (
+              <div></div>
+            ) : (
+              <div className="input">
+                <input placeholder="Your Name" type="text" />
+              </div>
+            )}
+
+            <div className="input">
+              <input type="email" placeholder="Your Email"></input>
+            </div>
+
+            <div className="input">
+              <input type="password" placeholder="Your Password"></input>
+            </div>
+          </div>
+          {action === "Sign Up" ? (
             <div></div>
           ) : (
-            <div className="input">
-              <input placeholder="Your Name" type="text" />
+            <div className="forget-password">
+              Forgot Password?
+              <span>Click Here</span>
             </div>
           )}
 
-          <div className="input">
-            <input type="email" placeholder="Your Email"></input>
-          </div>
-
-          <div className="input">
-            <input type="password" placeholder="Your Password"></input>
-          </div>
-        </div>
-        {action === "Sign Up" ? (
-          <div></div>
-        ) : (
-          <div className="forget-password">
-            Forgot Password?
-            <span>Click Here</span>
-          </div>
-        )}
-
-        <div className="submit-container">
-          <div
-            className={action === "Login" ? "submit gray" : "submit"}
-            onClick={() => {
-              setAction("SignUp");
-            }}
-          >
-            Sign Up
-          </div>
-          <div
-            className={action === "Sign Up" ? "submit gray" : "submit"}
-            onClick={() => {
-              setAction("Login");
-            }}
-          >
-            Login
-          </div>
-        </div>
-      </div>
-
-      <div className="form-container">
-        <div className="">
-          <hr />
-          <div className="google-button">
-            <GoogleButton
-              className="g-btn"
-              type="light"
-              onClick={handleGoogleSignIn}
-            />
-          </div>
-          <div>
-            Already have an account?
-            <Link
-              to="/login"
-              style={{
-                textDecoration: "none",
-                color: "var(--twitter-color)",
-                fontWeight: "600",
-                marginLeft: "5px",
+          <div className="submit-container">
+            <div
+              className={action === "Login" ? "submit gray" : "submit"}
+              onClick={() => {
+                setAction("SignUp");
               }}
             >
-              Log In
-            </Link>
+              Sign Up
+            </div>
+            <div
+              className={action === "Sign Up" ? "submit gray" : "submit"}
+              onClick={() => {
+                setAction("Login");
+              }}
+            >
+              Login
+            </div>
+          </div>
+        </div>
+
+        <div className="form-container">
+          <div className="">
+            <hr />
+            <div className="google-button">
+              <GoogleButton
+                className="g-btn"
+                type="light"
+                onClick={handleGoogleSignIn}
+              />
+            </div>
+            <div>
+              Already have an account?
+              <Link
+                to="/login"
+                style={{
+                  textDecoration: "none",
+                  color: "var(--twitter-color)",
+                  fontWeight: "600",
+                  marginLeft: "5px",
+                }}
+              >
+                Log In
+              </Link>
+            </div>
           </div>
         </div>
       </div>
