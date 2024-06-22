a6a1fc973f1d3dc0f782f574104a23a4aa64cccb
dwivedishrey <97790062+dwivedishrey@users.noreply.github.com>
2024-06-22 23:33:06 +0530

    final changes2

--- frontend/src/pages/Main/Components/About.js
+++ frontend/src/pages/Main/Components/About.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import { purple } from '@mui/material/colors';
 import "./Navbar.css";
+import { Link } from 'react-router-dom';
 
 
 const About = () => {
@@ -20,7 +21,7 @@ const About = () => {
       <div className='about-text'>
       <h1> Your Guide to the Basics</h1>
       <p>Starting your financial journey can be overwhelming, but with the right knowledge, you can confidently manage your finances and achieve your financial goals. ArthaMarga aims to equip you with the fundamental principles of personal finance.</p>
-      <a href='/login' className='about-btn'>Get Started</a>
+      <Link to='/login' className='about-btn'>Get Started</Link>
       </div>
     </div>
   )
